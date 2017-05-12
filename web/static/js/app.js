752743711f73d5ae14b01399cf99dee79052b821
Zach Sommers <github@zachsommers.com>
2017-05-12 11:27:16 -0700

    Add JS

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,44 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+//
+// Populate feeds
+//
+$(document).ready(function() {
+    $('div.flickr').flickrush({
+        limit: 6,
+        id: '44499772@N06',
+        random: true
+    });
+});
+
+
+//
+// Make the navigation dropdown for mobile
+//
+$(document).ready(function() {
+    // Build dropdown
+    $("<select />").appendTo(".navigation nav div div");
+
+    // Add dummy option
+    $("<option />", {
+        "selected": "selected",
+        "value": "",
+        "text": "Go to..."
+    }).appendTo("nav select");
+
+    // Populate dropdown from nav
+    $(".navigation nav li a").each(function() {
+        var el = $(this);
+        $("<option />", {
+            "value": el.attr("href"),
+            "text": el.text()
+        }).appendTo("nav select");
+    });
+
+    // Make dropdown functional
+    $("nav select").change(function () {
+        window.location = $(this).find("option:selected").val();
+    });
+});
